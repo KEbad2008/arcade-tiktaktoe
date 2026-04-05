f1c84ee3b81a3289efe78c8fb7ea1652640f8c96
KEbad2008 <ebaduk117@gmail.com>
2026-04-04 21:02:14 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,77 @@
+const cells = document.querySelectorAll(".cell");
+
+const statusText = document.getElementById("status");
+const restartBtn = document.getElementById("restart");
+
+const bgMusic = document.getElementById("bg-music");
+
+const clickSound = new Audio("clicksound.mp3");
+const winSound = new Audio("clicksound.mp3);
+
+let currentPlayer = "X";
+let board = Array(9).fill("");
+
+let running = true;
+
+const winConditions = [
+  [0,1,2], [3,4,5], [6,7,8],
+  [0,3,6], [1,4,7], [2,5,8],
+  [0,4,8], [2,4,6]
+];
+
+let musicStarted = false;
+
+document.addEventListener("click", () => {
+  if (!musicStarted) {
+    bgMusic.play().catch(err => console.log("Autoplay blocked until user interaction"));
+    musicStarted = true;
+  }
+}, {once: true });
+
+cells.forEach(cell => cell.addEventListener("click", handleClick));
+restartBtn.addEventListener("click", () => {
+  restartGame();
+  clickSound.play();
+});
+
+function handleClick() {
+  const index = this.dataset.index;
+  if (board[index] || !running) return;
+
+  board[index] = currentPlayer;
+  this.textContent = currentPlayer;
+  clickSound.currentTime = 0;
+  clickSound.play();
+  checkWinner();
+}
+
+function checkWinner() {
+  let won = winConditions.some(condition => {
+    const [a,b,c] = condition;
+    return board[a] && board[a] === board[b] && board[a] === board[c];
+  });
+
+  if (won) {
+    statusText.textContent = 'Player ${currentPlayer} wins!';
+    running = false;
+    winSound.play();
+    return;
+  }
+
+  if (!board.includes("")) {
+    statusText.textContent = "It is a draw";
+    running = false;
+    return;
+  }
+
+  currentPlayer = currentPlayer === "X" ? "0" : "X";
+  statusText.textContent = 'Player ${currentPlayer}'s turn';
+}
+
+function restartGame() {
+  board.fill("");
+  cells.forEach(cell => cell.textContent = "");
+  currentPlayer = "X";
+  running = true;
+  statusText.textContent = 'Player ${currentPlayer}'s turn';
+}
